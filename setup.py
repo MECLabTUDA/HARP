e6933b190c52bfa38224093ec77bd1a556a2c2c4
MECLabTUDA <47031657+MECLabTUDA@users.noreply.github.com>
2024-06-25 17:08:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HARPipe',
-    version='1.0.1',
+    version='1.0',
     packages=find_packages(),
     install_requires=[],  # List your project's dependencies here
     description='Histological Artifact Restoration Pipeline',
