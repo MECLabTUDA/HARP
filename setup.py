41d11791a8c433c0e6c375e8a1f300c1a8228d77
MECLabTUDA <47031657+MECLabTUDA@users.noreply.github.com>
2024-06-25 18:28:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HARPipe',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     python_requires='>=3.11.4',
     install_requires=[
